684af52c0a2e7b9afde97eb416db5850d3d86ac4
elmerjr13 <146602483+elmerjr13@users.noreply.github.com>
2024-02-18 12:48:25 -0600

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,15 @@
+const YesBtn = document.querySelector('#YesBtn');
+
+YesBtn.addEventListener('click',function() {
+    alert('Gracias por perdonarme')
+});
+
+const NoBtn = document.querySelector('#NoBtn');
+
+NoBtn.addEventListener('mouseover', function()  {
+    const randomX = parseInt(Math.random()*100);
+    const randomY =parseInt(Math.random()*100);
+    NoBtn.style.setProperty('top',randomY+'%');
+    NoBtn.style.setProperty('left',randomX+'%');
+    NoBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
